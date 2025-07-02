44a59fdf15fbe10128afca500ad32dd0d152037a
kevelscy <kevelscy@gmail.com>
2025-07-01 23:59:04 -0400

    feat: initialize Onse Conecta website with Astro and Tailwind CSS
    - Added package.json for project configuration and dependencies.
    - Created favicon and various icon assets for the website.
    - Implemented a welcome component with a background image and logo.
    - Configured SEO settings in a new seo.ts file.
    - Established a layout component for consistent page structure.
    - Developed the main index page to render the welcome component.
    - Included global CSS styles with Tailwind CSS configuration.
    - Set up TypeScript configuration for the project.

---
+++ src/config/seo.ts
@@ -0,0 +1,11 @@
+export const SEO_CONFIG = {
+  title: 'Onse Conecta',
+  website: 'https://onseconecta.com',
+  description: '¡Únete a nuestra comunidad de profesionales! ¿Eres carpintero, plomero o diseñador? ¡Muestra tus habilidades en Onse Conecta! Crea un perfil completo y comienza a recibir solicitudes de trabajo.',
+  url: 'https://onseconecta.com',
+  email: 'info@onseconecta.com',
+  keywords: ['onse conecta', 'empleos venezuela', 'trabajo venezuela', 'cartera de clientes'],
+  rrss: {
+    instagram: 'https://www.instagram.com/onseconecta'
+  }
+}
